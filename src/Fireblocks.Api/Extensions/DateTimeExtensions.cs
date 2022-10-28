930605fdcccaeb1f00ce3a2aa0f8d43be287ec64
Ed Wang <ed555009@gmail.com>
2022-10-28 15:42:27 +0800

    fix ToUnixEpochDate

--- src/Fireblocks.Api/Extensions/DateTimeExtensions.cs
+++ src/Fireblocks.Api/Extensions/DateTimeExtensions.cs
@@ -3,6 +3,5 @@ namespace Fireblocks.Api.Extensions;
 public static class DateTimeExtensions
 {
 	public static long ToUnixEpochDate(this DateTime dt) =>
-		(long)Math.Round((new DateTimeOffset(dt)
-					- new DateTimeOffset(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalSeconds);
+		dt.ToUniversalTime().Subtract(DateTime.UnixEpoch).Ticks / TimeSpan.TicksPerSecond;
 }
