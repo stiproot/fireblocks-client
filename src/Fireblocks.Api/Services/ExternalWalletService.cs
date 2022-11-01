c330cbe4e0fba04a7f632d1fff15c330acd984ce
Ed Wang <ed555009@gmail.com>
2022-11-01 15:09:35 +0800

    add ExternalWalletService & tests

---
+++ src/Fireblocks.Api/Services/ExternalWalletService.cs
@@ -0,0 +1,45 @@
+using Fireblocks.Api.Interfaces;
+using Fireblocks.Api.Models.Requests;
+using Fireblocks.Api.Models.Responses;
+using Refit;
+
+namespace Fireblocks.Api.Services;
+
+public class ExternalWalletService : IExternalWalletService
+{
+	private readonly IExternalWalletApi _externalWalletApi;
+
+	public ExternalWalletService(IExternalWalletApi externalWalletApi) =>
+		_externalWalletApi = externalWalletApi;
+
+	public async Task<ApiResponse<ExternalWalletAssetModel>> CreateExternalWalletAssetAsync(
+		string walletId,
+		string assetId,
+		CreateExternalWalletAssetModel payload) =>
+			await _externalWalletApi.CreateExternalWalletAssetAsync(walletId, assetId, payload);
+
+	public async Task<ApiResponse<ExternalWalletModel>> CreateExternalWalletAsync(CreateExternalWalletModel payload) =>
+		await _externalWalletApi.CreateExternalWalletAsync(payload);
+
+	public async Task<ApiResponse<string>> DeleteExternalWalletAssetAsync(string walletId, string assetId) =>
+		await _externalWalletApi.DeleteExternalWalletAssetAsync(walletId, assetId);
+
+	public async Task<ApiResponse<string>> DeleteExternalWalletAsync(string walletId) =>
+		await _externalWalletApi.DeleteExternalWalletAsync(walletId);
+
+	public async Task<ApiResponse<ExternalWalletAssetModel>> GetExternalWalletAssetAsync(
+		string walletId,
+		string assetId) =>
+			await _externalWalletApi.GetExternalWalletAssetAsync(walletId, assetId);
+
+	public async Task<ApiResponse<ExternalWalletModel>> GetExternalWalletAsync(string walletId) =>
+		await _externalWalletApi.GetExternalWalletAsync(walletId);
+
+	public async Task<ApiResponse<IEnumerable<ExternalWalletModel>>> GetExternalWalletsAsync() =>
+		await _externalWalletApi.GetExternalWalletsAsync();
+
+	public async Task<ApiResponse<string>> SetCustomerRefIdForExternalWalletAsync(
+		string walletId,
+		SetCustomerRefIdForExternalWalletModel payload) =>
+			await _externalWalletApi.SetCustomerRefIdForExternalWalletAsync(walletId, payload);
+}
---
+++ test/Fireblocks.Api.Tests/ExternalWalletServiceTests.cs
@@ -0,0 +1,152 @@
+namespace Fireblocks.Api.Tests;
+
+public class ExternalWalletServiceTests : BaseServiceTests
+{
+	private readonly Mock<IExternalWalletApi> _externalWalletApiMock;
+	private readonly IExternalWalletService _externalWalletService;
+
+	public ExternalWalletServiceTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
+	{
+		_externalWalletApiMock = new Mock<IExternalWalletApi>();
+		_externalWalletService = new ExternalWalletService(_externalWalletApiMock.Object);
+	}
+
+	[Fact]
+	public async Task CreateExternalWalletAssetAsync_ShouldSucceedAsync()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.CreateExternalWalletAssetAsync(
+				It.IsAny<string>(),
+				It.IsAny<string>(),
+				It.IsAny<Request.CreateExternalWalletAssetModel>()))
+			.Returns(CreateResponse<ExternalWalletAssetModel>(HttpStatusCode.OK));
+
+		// When
+		var result = await _externalWalletService.CreateExternalWalletAssetAsync(
+			"walletId",
+			"assetId",
+			new Request.CreateExternalWalletAssetModel());
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task CreateExternalWalletAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.CreateExternalWalletAsync(It.IsAny<Request.CreateExternalWalletModel>()))
+			.Returns(CreateResponse<ExternalWalletModel>(HttpStatusCode.OK));
+
+		// When
+		var result = await _externalWalletService.CreateExternalWalletAsync(new Request.CreateExternalWalletModel());
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task DeleteExternalWalletAssetAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.DeleteExternalWalletAssetAsync(It.IsAny<string>(), It.IsAny<string>()))
+			.Returns(Task.FromResult(new ApiResponse<string>(
+				new HttpResponseMessage(HttpStatusCode.OK),
+				default,
+				new RefitSettings())));
+
+		// When
+		var result = await _externalWalletService.DeleteExternalWalletAssetAsync("walletId", "assetId");
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task DeleteExternalWalletAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.DeleteExternalWalletAsync(It.IsAny<string>()))
+			.Returns(Task.FromResult(new ApiResponse<string>(
+				new HttpResponseMessage(HttpStatusCode.OK),
+				default,
+				new RefitSettings())));
+
+		// When
+		var result = await _externalWalletService.DeleteExternalWalletAsync("walletId");
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task GetExternalWalletAssetAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.GetExternalWalletAssetAsync(It.IsAny<string>(), It.IsAny<string>()))
+			.Returns(CreateResponse<ExternalWalletAssetModel>(HttpStatusCode.OK));
+
+		// When
+		var result = await _externalWalletService.GetExternalWalletAssetAsync("walletId", "assetId");
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task GetExternalWalletAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.GetExternalWalletAsync(It.IsAny<string>()))
+			.Returns(CreateResponse<ExternalWalletModel>(HttpStatusCode.OK));
+
+		// When
+		var result = await _externalWalletService.GetExternalWalletAsync("walletId");
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task GetExternalWalletsAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.GetExternalWalletsAsync())
+			.Returns(CreateIEnumerableResponse<ExternalWalletModel>(HttpStatusCode.OK));
+
+		// When
+		var result = await _externalWalletService.GetExternalWalletsAsync();
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+
+	[Fact]
+	public async Task SetCustomerRefIdForExternalWalletAsync_ShouldSucceed()
+	{
+		// Given
+		_externalWalletApiMock
+			.Setup(x => x.SetCustomerRefIdForExternalWalletAsync(
+				It.IsAny<string>(),
+				It.IsAny<Request.SetCustomerRefIdForExternalWalletModel>()))
+			.Returns(Task.FromResult(new ApiResponse<string>(
+				new HttpResponseMessage(HttpStatusCode.OK),
+				default,
+				new RefitSettings())));
+
+		// When
+		var result = await _externalWalletService.SetCustomerRefIdForExternalWalletAsync(
+			"walletId",
+			new Request.SetCustomerRefIdForExternalWalletModel());
+
+		// Then
+		Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+	}
+}
