74061b40927b4543ed3aeabbcb27dac7f2b3f311
Simon Stipcich <simon.stipcich@derivco.co.za>
2025-07-03 08:33:22 +0200

    chore: remove commented out lines.

--- src/Fireblocks.Console/ServiceProviderBuilder.cs
+++ src/Fireblocks.Console/ServiceProviderBuilder.cs
@@ -1,7 +1,4 @@
-// using Fireblocks.Client.Configs;
 using Fireblocks.Client.Extensions;
-// using Fireblocks.Client.Abstractions;
-// using Fireblocks.Client.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,24 +17,7 @@ public static class ServiceProviderFactory
 
     var services = CreateServiceCollection();
 
-    // var apiConfig = new FireblocksApiConfig
-    // {
-    //   BaseUrl = configuration["Fireblocks:ApiConfig:BaseUrl"]!,
-    //   Version = configuration["Fireblocks:ApiConfig:Version"]!,
-    //   ApiKey = configuration["Fireblocks:ApiConfig:ApiKey"]!,
-    //   SecretKey = configuration["Fireblocks:ApiConfig:ApiSecret"]!,
-    // };
-
-    // var jwtConfig = new JwtConfig
-    // {
-    //   ExpiredInSeconds = int.Parse(configuration["Fireblocks:JwtConfig:ExpiredInSeconds"]!)
-    // };
-
-    // services.AddSingleton<IFireblocksApiConfig>(apiConfig);
-    // services.AddSingleton<IJwtConfig>(jwtConfig);
-
     services.AddSingleton<IConfiguration>(configuration);
-    // services.AddSingleton<ITokenService, TokenService>();
     services
       .AddFireblocksApiConfigs(configuration)
       .AddFireblocksApiServices(configuration);
